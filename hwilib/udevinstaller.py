3352940c3ef567cba7e04cdb23dc6ddb5547c91e
Andrew Chow <achow101-github@achow101.com>
2020-08-27 13:54:58 -0400

    Typecheck udevinstaller.py

--- hwilib/udevinstaller.py
+++ hwilib/udevinstaller.py
@@ -3,9 +3,14 @@ from .errors import NEED_TO_BE_ROOT
 from shutil import copy
 from os import path, listdir, getlogin, geteuid
 
+from typing import (
+    Dict,
+    Union,
+)
+
 class UDevInstaller(object):
     @staticmethod
-    def install(source, location):
+    def install(source: str, location: str) -> Dict[str, Union[int, str]]:
         try:
             udev_installer = UDevInstaller()
             udev_installer.copy_udev_rule_files(source, location)
@@ -18,41 +23,41 @@ class UDevInstaller(object):
             raise
         return {"success": True}
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._udevadm = '/sbin/udevadm'
         self._groupadd = '/usr/sbin/groupadd'
         self._usermod = '/usr/sbin/usermod'
 
-    def _execute(self, command, *args):
-        command = [command] + list(args)
-        check_call(command, stderr=DEVNULL, stdout=DEVNULL)
+    def _execute(self, command: str, *args: str) -> None:
+        commands = [command] + list(args)
+        check_call(commands, stderr=DEVNULL, stdout=DEVNULL)
 
-    def trigger(self):
+    def trigger(self) -> None:
         self._execute(self._udevadm, 'trigger')
 
-    def reload_rules(self):
+    def reload_rules(self) -> None:
         self._execute(self._udevadm, 'control', '--reload-rules')
 
-    def add_user_plugdev_group(self):
+    def add_user_plugdev_group(self) -> None:
         self._create_group('plugdev')
         self._add_user_to_group(getlogin(), 'plugdev')
 
-    def _create_group(self, name):
+    def _create_group(self, name: str) -> None:
         try:
             self._execute(self._groupadd, name)
         except CalledProcessError as e:
             if e.returncode != 9: # group already exists
                 raise
 
-    def _add_user_to_group(self, user, group):
+    def _add_user_to_group(self, user: str, group: str) -> None:
         self._execute(self._usermod, '-aG', group, user)
 
-    def copy_udev_rule_files(self, source, location):
+    def copy_udev_rule_files(self, source: str, location: str) -> None:
         src_dir_path = source
         for rules_file_name in listdir(_resource_path(src_dir_path)):
             if '.rules' in rules_file_name:
                 rules_file_path = _resource_path(path.join(src_dir_path, rules_file_name))
                 copy(rules_file_path, location)
 
-def _resource_path(relative_path):
+def _resource_path(relative_path: str) -> str:
     return path.join(path.dirname(__file__), relative_path)
