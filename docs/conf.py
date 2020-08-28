22d694547cbed49be62105db93c4b6041264490e
Andrew Chow <achow101-github@achow101.com>
2020-08-27 23:00:52 -0400

    Configure readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import sphinx_rtd_theme
+
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +35,7 @@ release = '1.1.2'
 extensions = [
     "sphinx.ext.autodoc",
     "sphinxcontrib.autoprogram",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -64,3 +67,6 @@ autodoc_member_order = "bysource"
 
 # Show both class and init docstring
 autoclass_content = "both"
+
+# Mock these imports
+autodoc_mock_imports = ["hid", "ecdsa", "pyaes", "mnemonic", "typing_extensions", "usb1", "PySide2"]
