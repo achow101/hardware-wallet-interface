bitcoin-core/HWI

Add Descriptor.expand to get the scripts for a descriptor

hwilib/descriptor.py
@@ -1,5 +1,8 @@
 from .key import ExtendedKey, KeyOriginInfo, parse_path
+from .serializations import hash160, sha256
 
+from binascii import unhexlify
+from collections import namedtuple
 from enum import Enum
 from typing import (
     List,
@@ -8,6 +11,8 @@ from typing import (
 )
 
 # From: https://github.com/bitcoin/bitcoin/blob/master/src/script/descriptor.cpp
+
+ExpandedScripts = namedtuple("ExpandedScripts", ["output_script", "redeem_script", "witness_script"])
 
 def PolyMod(c: int, val: int) -> int:
     c0 = c >> 35
@@ -105,6 +110,19 @@ class PubkeyProvider(object):
             s += self.deriv_path
         return s
 
+    def get_pubkey_bytes(self, pos: int) -> bytes:
+        if self.extkey is not None:
+            if self.deriv_path is not None:
+                path_str = self.deriv_path[1:]
+                if path_str[-1] == "*":
+                    path_str = path_str[-1] + str(pos)
+                path = parse_path(path_str)
+                child_key = self.extkey.derive_pub_path(path)
+                return child_key.pubkey
+            else:
+                return self.extkey.pubkey
+        return unhexlify(self.pubkey)
+
     def __lt__(self, other: 'PubkeyProvider') -> bool:
         return self.pubkey < other.pubkey
 
@@ -130,6 +148,12 @@ class Descriptor(object):
     def to_string(self) -> str:
         return AddChecksum(self.to_string_no_checksum())
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        """
+        Returns the scripts for a descriptor at the given `pos` for ranged descriptors.
+        """
+        raise NotImplementedError("The Descriptor base class does not implement this method")
+
 
 class PKHDescriptor(Descriptor):
     def __init__(
@@ -138,6 +162,10 @@ class PKHDescriptor(Descriptor):
     ) -> None:
         super().__init__([pubkey], None, "pkh")
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        script = b"\x76\xa9\x14" + hash160(self.pubkeys[0].get_pubkey_bytes(pos)) + b"\x88\xac"
+        return ExpandedScripts(script, None, None)
+
 
 class WPKHDescriptor(Descriptor):
     def __init__(
@@ -145,6 +173,10 @@ class WPKHDescriptor(Descriptor):
         pubkey: 'PubkeyProvider'
     ) -> None:
         super().__init__([pubkey], None, "wpkh")
+
+    def expand(self, pos: int) -> "ExpandedScripts":
+        script = b"\x00\x14" + hash160(self.pubkeys[0].get_pubkey_bytes(pos))
+        return ExpandedScripts(script, None, None)
 
 
 class MultisigDescriptor(Descriptor):
@@ -162,6 +194,17 @@ class MultisigDescriptor(Descriptor):
     def to_string_no_checksum(self) -> str:
         return "{}({},{})".format(self.name, self.thresh, ",".join([p.to_string() for p in self.pubkeys]))
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        m = (self.thresh + 0x50).to_bytes(1, "big") if self.thresh > 0 else b"\x00"
+        n = (len(self.pubkeys) + 0x50).to_bytes(1, "big") if len(self.pubkeys) > 0 else b"\x00"
+        script: bytes = m
+        for p in self.pubkeys:
+            pk = p.get_pubkey_bytes(pos)
+            script += len(pk).to_bytes(1, "big") + pk
+        script += m + b"\xae"
+
+        return ExpandedScripts(script, None, None)
+
 
 class SHDescriptor(Descriptor):
     def __init__(
@@ -170,6 +213,12 @@ class SHDescriptor(Descriptor):
     ) -> None:
         super().__init__([], subdescriptor, "sh")
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        assert self.subdescriptor
+        redeem_script, _, witness_script = self.subdescriptor.expand(pos)
+        script = b"\xa9\x14" + hash160(redeem_script) + b"\x87"
+        return ExpandedScripts(script, redeem_script, witness_script)
+
 
 class WSHDescriptor(Descriptor):
     def __init__(
@@ -177,6 +226,12 @@ class WSHDescriptor(Descriptor):
         subdescriptor: Optional['Descriptor']
     ) -> None:
         super().__init__([], subdescriptor, "wsh")
+
+    def expand(self, pos: int) -> "ExpandedScripts":
+        assert self.subdescriptor
+        witness_script, _, _ = self.subdescriptor.expand(pos)
+        script = b"\x00\x20" + sha256(witness_script)
+        return ExpandedScripts(script, None, witness_script)
 
 
 def _get_func_expr(s: str) -> Tuple[str, str]:

achow101/hardware-wallet-interface

hwilib/descriptor.py
@@ -1,5 +1,8 @@
 from .key import ExtendedKey, KeyOriginInfo, parse_path
+from .serializations import hash160, sha256
 
+from binascii import unhexlify
+from collections import namedtuple
 from enum import Enum
 from typing import (
     List,
@@ -8,6 +11,8 @@ from typing import (
 )
 
 # From: https://github.com/bitcoin/bitcoin/blob/master/src/script/descriptor.cpp
+
+ExpandedScripts = namedtuple("ExpandedScripts", ["output_script", "redeem_script", "witness_script"])
 
 def PolyMod(c: int, val: int) -> int:
     c0 = c >> 35
@@ -105,6 +110,19 @@ class PubkeyProvider(object):
             s += self.deriv_path
         return s
 
+    def get_pubkey_bytes(self, pos: int) -> bytes:
+        if self.extkey is not None:
+            if self.deriv_path is not None:
+                path_str = self.deriv_path[1:]
+                if path_str[-1] == "*":
+                    path_str = path_str[-1] + str(pos)
+                path = parse_path(path_str)
+                child_key = self.extkey.derive_pub_path(path)
+                return child_key.pubkey
+            else:
+                return self.extkey.pubkey
+        return unhexlify(self.pubkey)
+
     def get_full_derivation_path(self, pos: int) -> str:
         """
         Returns the full derivation path at the given position, including the origin
@@ -140,6 +158,12 @@ class Descriptor(object):
     def to_string(self) -> str:
         return AddChecksum(self.to_string_no_checksum())
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        """
+        Returns the scripts for a descriptor at the given `pos` for ranged descriptors.
+        """
+        raise NotImplementedError("The Descriptor base class does not implement this method")
+
 
 class PKHDescriptor(Descriptor):
     def __init__(
@@ -148,6 +172,10 @@ class PKHDescriptor(Descriptor):
     ) -> None:
         super().__init__([pubkey], None, "pkh")
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        script = b"\x76\xa9\x14" + hash160(self.pubkeys[0].get_pubkey_bytes(pos)) + b"\x88\xac"
+        return ExpandedScripts(script, None, None)
+
 
 class WPKHDescriptor(Descriptor):
     def __init__(
@@ -155,6 +183,10 @@ class WPKHDescriptor(Descriptor):
         pubkey: 'PubkeyProvider'
     ) -> None:
         super().__init__([pubkey], None, "wpkh")
+
+    def expand(self, pos: int) -> "ExpandedScripts":
+        script = b"\x00\x14" + hash160(self.pubkeys[0].get_pubkey_bytes(pos))
+        return ExpandedScripts(script, None, None)
 
 
 class MultisigDescriptor(Descriptor):
@@ -172,6 +204,17 @@ class MultisigDescriptor(Descriptor):
     def to_string_no_checksum(self) -> str:
         return "{}({},{})".format(self.name, self.thresh, ",".join([p.to_string() for p in self.pubkeys]))
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        m = (self.thresh + 0x50).to_bytes(1, "big") if self.thresh > 0 else b"\x00"
+        n = (len(self.pubkeys) + 0x50).to_bytes(1, "big") if len(self.pubkeys) > 0 else b"\x00"
+        script: bytes = m
+        for p in self.pubkeys:
+            pk = p.get_pubkey_bytes(pos)
+            script += len(pk).to_bytes(1, "big") + pk
+        script += n + b"\xae"
+
+        return ExpandedScripts(script, None, None)
+
 
 class SHDescriptor(Descriptor):
     def __init__(
@@ -180,6 +223,12 @@ class SHDescriptor(Descriptor):
     ) -> None:
         super().__init__([], subdescriptor, "sh")
 
+    def expand(self, pos: int) -> "ExpandedScripts":
+        assert self.subdescriptor
+        redeem_script, _, witness_script = self.subdescriptor.expand(pos)
+        script = b"\xa9\x14" + hash160(redeem_script) + b"\x87"
+        return ExpandedScripts(script, redeem_script, witness_script)
+
 
 class WSHDescriptor(Descriptor):
     def __init__(
@@ -187,6 +236,12 @@ class WSHDescriptor(Descriptor):
         subdescriptor: Optional['Descriptor']
     ) -> None:
         super().__init__([], subdescriptor, "wsh")
+
+    def expand(self, pos: int) -> "ExpandedScripts":
+        assert self.subdescriptor
+        witness_script, _, _ = self.subdescriptor.expand(pos)
+        script = b"\x00\x20" + sha256(witness_script)
+        return ExpandedScripts(script, None, witness_script)
 
 
 def _get_func_expr(s: str) -> Tuple[str, str]: